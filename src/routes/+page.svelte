3bcf53642b135305a04eefbc52b09770c951d164
Uncle Common <krishnerkar@gmail.com>
2025-12-30 20:37:10 +0000

    Remove MidCTA section from homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import Header from "$lib/components/Header.svelte";
 	import Hero from "$lib/components/Hero.svelte";
 	import About from "$lib/components/About.svelte";
-	import MidCTA from "$lib/components/MidCTA.svelte";
 	import FAQ from "$lib/components/FAQ.svelte";
 	import Contact from "$lib/components/Contact.svelte";
 </script>
@@ -12,7 +11,6 @@
 <main>
 	<Hero />
 	<About />
-	<MidCTA />
 	<FAQ />
 	<Contact />
 </main>
